62182f052e3646aa722daea0b9a2c088212bed8f
AnthonyWillocqCondorcet <anthony.willocq@condorcet.be>
2026-06-10 10:14:53 +0200

    HOTFIX, correction soucis base de donnée qui pointé vers le docker & pas sur la base de donnée postgress

--- app/__init__.py
+++ app/__init__.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = '81104011751ac9f2e6f46c45f2dfba32'
 #supprime les notifications inutiles
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://anonyme:anonyme@localhost:5432/Brasserie"
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://anonyme:anonyme@host.docker.internal:5432/Brasserie"
 
 db = SQLAlchemy(app)
 
